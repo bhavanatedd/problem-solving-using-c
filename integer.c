9514fbe6ac764428cd3a244c860b3ddda25cb75f
bhavanatedd <bhavanag495@gmail.com>
2024-12-02 09:51:11 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    clrscr();
+    printf("maximum integer value :%d\n",INT_MAX);
+    printf("minimum integer value :%d\n",INT_MIN);
+    printf("maximum integer value :%d\n",LONG_MAX);
+    printf("minimum integer value :%d\n",LONG_MIN);
+    getch();
+    return 0;
+}
